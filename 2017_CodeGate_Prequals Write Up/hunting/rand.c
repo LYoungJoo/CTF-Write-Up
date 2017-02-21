2778c503652b6b05d6582365b35639b7299c0e94
LeeYoungJoo <boright77@naver.com>
2017-02-21 19:49:56 +0900

    Create rand.c

---
+++ 2017_CodeGate_Prequals Write Up/hunting/rand.c
@@ -0,0 +1,14 @@
+// http://nextline.tistory.com/110
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < 100; ++i)
+        printf("%d\n", rand() & 3);
+    return 0;
+}
